5f634dfa3de4a5fd446de7c542742f5d010eda5e
br4ska <sven.steffan@gmail.com>
2018-05-10 15:59:22 +0200

    Reorganized project-structure for npm-publishing to a private npm-registry

---
+++ webpack.js
@@ -0,0 +1,18 @@
+﻿module.exports = {
+    entry: './src/js/nevsuite.app.root.js',
+    output: {
+        filename: 'nevsuite.bundle.js',
+        path: './dist'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+};
